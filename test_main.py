4ebce286ad6dc3e6006ca11fafb03908a4262b3b
dimitrieidifesk <123076304+dimitrieidifesk@users.noreply.github.com>
2023-01-20 17:22:30 +0300

    Дополнил метод greetings в app/main.py

---
+++ test_main.py
@@ -0,0 +1,16 @@
+import pytest
+from main import *
+
+
+@pytest.mark.parametrize( 'name,expected', [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],)
+def test_greeting(name: str, expected: str):
+    """Текст приветствия зависит от имени."""
+    assert greeting(name) == expected
+
+def test_capitalize():
+    """Все слова в имени начинаются с большой буквы."""
+    name = 'яндекс практикум'
+    assert greeting(name) == 'Привет, Яндекс Практикум'
+
+
+
