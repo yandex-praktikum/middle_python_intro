479a1defe4b205de2a29fcbbe3e975640d306ec3
igms84 <kontekst.seo@yandex.ru>
2023-03-06 13:49:13 +0300

    Update test_main.py
    Исправлен регистр проверочной строки

--- app/test_main.py
+++ app/test_main.py
@@ -2,12 +2,14 @@ import pytest
 from main import *
 
 
-@pytest.mark.parametrize( 'name,expected', [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],)
+@pytest.mark.parametrize("name,expected", [('Никита', 'Привет, Никита'),
+                                           ('Ольга', 'Привет, Ольга')], )
 def test_greeting(name: str, expected: str):
     """Текст приветствия зависит от имени."""
     assert Greeting(name) == expected
 
+
 def test_capitalize():
     """Все слова в имени начинаются с большой буквы."""
-    name = 'яндекс практикум'
+    name = 'Яндекс Практикум'
     assert Greeting(name) == 'Привет, Яндекс Практикум'
